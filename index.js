4b553b9ba457de55664da3c8c4185e575f2fd8c6
jiangbodev <jiangbo.dev@gmail.com>
2017-10-29 09:06:51 +0800

    update date format

--- index.js
+++ index.js
@@ -137,7 +137,5 @@ function time (start) {
 }
 
 function getFormattedDate() {
-    var date = new Date();
-    var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-    return str;
+   return new Date().toJSON().substring(0,19).replace('T',' ');
 }
